06d96d8e9e57dadff3774458763f95d213d839d0
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-04 17:17:39 +0100

    code refactoring

--- src/Reducer/RootReducer.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import {BurgerReducer} from "./BurgerReducer";
-import {BurgerState} from "../State/BurgerState";
-
-export const rootReducer = combineReducers({
-    burger: BurgerReducer,
-})
-
-export type RootState = ReturnType<typeof rootReducer>
